f99f6b7491a946a5686cc01e20883fb60cf0fdf7
Lachlan <lachlan.miller.1990@outlook.com>
2018-06-14 14:06:59 +0900

    Add the start of integration test

--- test/integration/parses-a-snippet-by-hash.test.js
+++ test/integration/parses-a-snippet-by-hash.test.js
@@ -1,10 +1,18 @@
 const exec = require('child_process').exec
 
-const markdownWithHashSnippet =  ''
-
 const context = (msg, cb) => describe(msg, cb)
 
 describe("integration tests", () => {
   context("snippet by a commit hash", () => {
+    it('works', () => {
+      exec(
+        "node parse.js resources/articles/article-with-line-numbers-snippet" , 
+        (err, stdout, stderr) => {
+          if (err) console.log(err)
+          if (stderr) console.log(stderr)
+
+          console.log(stdout)
+        })
+    })
   })
 })
