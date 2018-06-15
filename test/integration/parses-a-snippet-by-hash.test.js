dbe7f0943d5a9a15dc5e78df8bc6b51a64fa10a9
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-06-15 23:46:11 +0900

    update

--- test/integration/parses-a-snippet-by-hash.test.js
+++ test/integration/parses-a-snippet-by-hash.test.js
@@ -6,26 +6,24 @@ const context = (msg, cb) => describe(msg, cb)
 
 const expected = fs.readFileSync(path.join(__dirname, "..", "..", "resources", "articles", "article-with-hash-snippet", "expected-output.md"), "utf8")
 
-xdescribe("integration tests", () => {
-  context("snippet by a commit hash", () => {
-    it("outputs the correct markdown", (done) => {
-      expect.assertions(1)
-      exec("node src/index.js ../resources/articles/article-with-hash-snippet/article.md",
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log(err)
-            return
-          }
-          if (stderr) {
-            console.log(stderr)
-            return
-          }
+describe("integration tests", () => {
+  it("outputs the correct markdown", (done) => {
+    expect.assertions(1)
+    exec("node src/index.js ../resources/articles/article-with-hash-snippet/article.md",
+      (err, stdout, stderr) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        if (stderr) {
+          console.log(stderr)
+          return
+        }
 
-          const actual = fs.readFileSync(path.join(__dirname, "..", "..", "resources", "articles", "article-with-hash-snippet", "actual.md"), "utf8")
+        const actual = fs.readFileSync(path.join(__dirname, "..", "..", "resources", "articles", "article-with-hash-snippet", "actual.md"), "utf8")
 
-          expect(actual).toBe(expected)
-          done()
-        })
-    })
+        expect(actual).toBe(expected)
+        done()
+      })
   })
 })
